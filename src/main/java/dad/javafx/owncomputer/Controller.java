73378cc9331f9935f18ee8bf37b6c5517065e260
Ayoamaro <ayoze.amaro@gmail.com>
2021-01-31 19:17:11 +0000

    Actualización de la vista y controlador

--- src/main/java/dad/javafx/owncomputer/Controller.java
+++ src/main/java/dad/javafx/owncomputer/Controller.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,7 +18,7 @@ public class Controller implements Initializable {
 	@FXML
 	private BorderPane view;
 	@FXML
-	private Button cpuBTN, motherboardBTN, heatsinkBTN, memoryramBTN, graphiccardBTN, harddiskBTN, accessoriesBTN, computerBTN, mountingBTN, addBTN, 
+	private Button cpuBTN, motherboardBTN, heatsinkBTN, memoryramBTN, graphiccardBTN, harddiskBTN, powersupplyBTN, caseBTN, devicesBTN, addBTN, 
 	quantityBTN, deleteBTN, priceBTN, discountBTN, taxesBTN, purchaseBTN;
 	@FXML
 	private ListView<String> productsLIST;
@@ -33,6 +34,39 @@ public class Controller implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) { }
 		
+	
+	@FXML
+	void onCPU_Pressed(ActionEvent event) { }
+	@FXML
+	void onMotherboard_Pressed(ActionEvent event) { }
+	@FXML
+	void onHeatSkin_Pressed(ActionEvent event) { }
+	@FXML
+	void onRAM_Pressed(ActionEvent event) { }
+	@FXML
+	void onGraphicCard_Pressed(ActionEvent event) { }
+	@FXML
+	void onMemory_Pressed(ActionEvent event) { }
+	@FXML
+	void onPowerSupply_Pressed(ActionEvent event) { }
+	@FXML
+	void onCase_Pressed(ActionEvent event) { }
+	@FXML
+	void onDevices_Pressed(ActionEvent event) { }
+	@FXML
+	void changeCuantity(ActionEvent event) { }
+	@FXML
+	void removeProduct(ActionEvent event) { }
+	@FXML
+	void changePrice(ActionEvent event) { }
+	@FXML
+	void applyDiscounts(ActionEvent event) { }
+	@FXML
+	void applyTaxes(ActionEvent event) { }
+	@FXML
+	void onPayAction(ActionEvent event) { }
+	
+	
 	// SHOW VIEW
 	public BorderPane getView() {
 		return view;
