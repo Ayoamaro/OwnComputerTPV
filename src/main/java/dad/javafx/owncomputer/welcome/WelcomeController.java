542a197184412cb50d180e865427fdf86819e41e
Ayoamaro <ayoze.amaro@gmail.com>
2021-02-09 22:11:43 +0000

    Login finished

--- src/main/java/dad/javafx/owncomputer/welcome/WelcomeController.java
+++ src/main/java/dad/javafx/owncomputer/welcome/WelcomeController.java
@@ -2,17 +2,25 @@ package dad.javafx.owncomputer.welcome;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import dad.javafx.util.DBUtils;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class WelcomeController implements Initializable {
+	
+	// MODEL
+	private Model model = new Model();
+	private ArrayList<String> linesCSV = new ArrayList<>();
 	
 	// VIEW
 	@FXML
@@ -20,7 +28,9 @@ public class WelcomeController implements Initializable {
 	@FXML
 	private Button loginBTN, resetBTN;
 	@FXML
-	private TextField usercodeTXT;
+	private TextField userTXT;
+	@FXML
+	private PasswordField pswdTXT;
 	
 
 	// CONSTRUCTOR
@@ -32,17 +42,40 @@ public class WelcomeController implements Initializable {
 
 	// INITIALIZE
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {  }
-	
+	public void initialize(URL location, ResourceBundle resources) { 
+		DBUtils.readUsersFile(linesCSV);
+		
+		Bindings.bindBidirectional(userTXT.textProperty(), model.userProperty());
+		Bindings.bindBidirectional(pswdTXT.textProperty(), model.pswdProperty());
+	}
 	
 	@FXML
 	void onLogin(ActionEvent event) { 
-		App.goToOwnComputer();
+		Boolean verify = false;
+		String md5Password = model.getPswd();
+		String md5User = model.getUser();
+		
+		for (int i = 0; i < linesCSV.size(); i++) {
+			String[] dataFile = linesCSV.get(i).split(",");
+		    String userEach = dataFile[0];
+		    String passEach = dataFile[1];
+
+		    if (userEach.equals(md5User) && passEach.equals(md5Password)) { 
+		    	verify = true; 
+		    }
+		}
+		
+		if (verify == true) { 
+			App.goToOwnComputer(); 
+		} else {
+			DBUtils.wrongCredentials();
+		}
 	}
 	
 	@FXML
 	void onReset(ActionEvent event) { 
-		usercodeTXT.setText("");
+		userTXT.setText("");
+		pswdTXT.setText("");
 	}
 	
 	// SHOW VIEW
--- src/main/java/dad/javafx/util/DBUtils.java
+++ src/main/java/dad/javafx/util/DBUtils.java
@@ -1,9 +1,49 @@
 package dad.javafx.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 /**
  * @author Ayoze Amaro
  *
  */
 public class DBUtils {
 
+	public static void readUsersFile(ArrayList<String> linesCSV) {
+		try {
+		    File file = new File ("src/main/resources/database/users.csv");
+		    FileReader readFile = new FileReader(file, Charset.forName("UTF-8"));
+		    BufferedReader usersRead = new BufferedReader(readFile);
+		    String lines;
+		    
+		    while ((lines = usersRead.readLine()) != null) {
+		    	linesCSV.add(lines);
+		    }
+		    usersRead.close();
+		}
+		catch (IOException error) { 
+			error.printStackTrace();
+		}
+	}
+	
+	public static void wrongCredentials() {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Wrong credentials");
+		alert.setHeaderText(null);
+		alert.setContentText("Ooops, there was an error! \n" + "Invalid email or password. Try again, please");
+		
+		Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image("/images/owncomputer-icon.png"));
+		
+		alert.showAndWait();
+	}
 }
