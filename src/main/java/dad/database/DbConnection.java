3238d19e86424266e0444643359bc48db2e85844
Ainalem93 <60430539+Ainalem93@users.noreply.github.com>
2021-02-17 21:48:17 +0000

    ready connection DB and read first CPU table

---
+++ src/main/java/dad/database/DbConnection.java
@@ -0,0 +1,22 @@
+package dad.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+		public static Connection connect() {
+			Connection con = null;
+			try {
+				Class.forName("org.sqlite.JDBC");
+				con = DriverManager.getConnection("jdbc:slite:ComponentsBD.db");
+				System.out.println("Connected!");
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e+"");
+			}
+			
+			return con;
+		}
+}
